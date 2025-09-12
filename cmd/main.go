5f69e2abb17db03844ec29fbd1aae220b1a4eb47
Сергей Хопин <sergejhopin@Air-Sergej.Dlink>
2025-09-12 10:27:36 +0300

    поднял сервак на gin

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+
 	"github.com/SergoHop/log-analyzer/internal/database"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	DB := db.Init()
 	fmt.Println("бд норм", DB != nil)
 	log.Println("бд конект")
+
+	r := gin.Default()
+	
+
+	r.GET("/health", func(c *gin.Context) {
+		c.String(http.StatusOK, "все норм")
+	})
+	r.Run(":8080")
 }
